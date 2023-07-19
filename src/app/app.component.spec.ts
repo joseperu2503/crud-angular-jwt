7cf8eb6ab844c405a1d97e6ec554337d8716ffb9
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-07-19 18:17:13 -0500

    cambiando nombre de proyecto y agregando .htaccess para deploy en apache

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'login-angular'`, () => {
+  it(`should have as title 'crud-angular-jwt'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-angular');
+    expect(app.title).toEqual('crud-angular-jwt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('login-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-angular-jwt app is running!');
   });
 });
